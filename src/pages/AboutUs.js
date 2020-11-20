144412c07aa5ce81e75044d752b2e6d0e99c0081
Tass2T <brunog90@gmail.com>
2020-11-20 14:40:27 +0100

    few styling done with styled component

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,13 @@
 import AboutSection from '../components/AboutSection'
+import FaqSection from '../components/FaqSection'
+import ServicesSection from '../components/ServicesSection'
 
 export default function AboutUs () {
     return (
-        <AboutSection/>
+        <>
+            <AboutSection/>
+            <ServicesSection/>
+            <FaqSection/>
+        </>
     )
 }
