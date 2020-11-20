612c653ccb05d6cf36f89e579b8bd71ec55369cc
Tass2T <brunog90@gmail.com>
2020-11-20 18:30:13 +0100

    router set

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import AboutUs from './pages/AboutUs'
 // import globalStyled
 import GlobalStyle from './components/GlobalStyle'
 import Nav from './components/Nav';
+import {Switch, Route} from 'react-router-dom'
+import ContactUs from './components/ContactUs';
+import OurWork from './components/OurWork';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
       <Nav/>
-      <AboutUs/>
+      <Switch>
+        <Route exact path="/">
+          <AboutUs/>
+        </Route>
+        <Route path="/contact">
+          <ContactUs/>
+        </Route>
+        <Route path="/work">
+          <OurWork/>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/OurWork.js
@@ -0,0 +1,11 @@
+
+
+const OurWork = () => {
+    return (
+        <div>
+            <h1>Out Work</h1>
+        </div>
+    )
+}
+
+export default OurWork
